c79865ee0a794a8a27f412f35a6eca06fafcee40
Héctor Romero López <hector@hromerol.es>
2024-10-24 12:54:42 +0200

    ADD: function to create HTML Element

--- script.js
+++ script.js
@@ -8,10 +8,38 @@ const getCharacterListFromApi = async () => {
         const caracterInformationFromApi = await response.json()
         console.log(caracterInformationFromApi)
         console.log(caracterInformationFromApi.results)
+        caracterInformationFromApi.results.map((characterInformation, index)=>{
+            console.log(characterInformation)
+        })
     }
     else {
         alert("Problemas con la api")
    }
 }
 
-getCharacterListFromApi()
+const createCharacterCardInHtml = (characterInformation) => {
+
+    const characterSection = document.getElementById("root")
+
+    const articleContainer = document.createElement("article")
+    const imageElement = document.createElement("img")
+    imageElement.src = characterInformation.image
+    const containerForText = document.createElement("div")
+    const NameCharacterTitle = docuemtn.createElement("h4")
+    NameCharacterTitle.innerText = characterInformation.name
+    const WorldForCharacter = document.createElement("p")
+    WorldForCharacter.innerText = characterInformation.origin.name
+    const CurrentWorkd = document.createElement("p")
+    CurrentWorkd.innerText = characterInformation.location.name
+
+    containerForText.appendChild(NameCharacterTitle)
+    containerForText.appendChild(`Planeta: ${WorldForCharacter}`)
+    containerForText.appendChild(`Localización: ${CurrentWorkd}`)
+
+    articleContainer.appendChild(imageElement)
+    articleContainer.appendChild(containerForText)
+
+
+    characterSection.appendChild(articleContainer)
+    
+}
